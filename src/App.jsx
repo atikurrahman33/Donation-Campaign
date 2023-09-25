98e7624574cf73d1580486070fafce2592300c0d
atikurrahman33 <foysalatik33@gmail.com>
2023-09-25 22:53:33 +0600

    fake data create

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 
 import './App.css'
+import Navbar from './component/navbar/Navbar'
+
+
 
 function App() {
  
@@ -8,6 +11,9 @@ function App() {
     <>
      
       <h1>Vite + React</h1>
+      <input type="radio" name="radio-4" className="radio radio-accent" checked />
+<input type="radio" name="radio-4" className="radio radio-accent" />
+<Navbar></Navbar>
       
     </>
   )
---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>i love you</h2>
+        </div>
+    );
+};
+
+export default Navbar;
