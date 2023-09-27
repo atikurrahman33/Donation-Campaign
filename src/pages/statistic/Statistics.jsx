fcd4f8b4f82d187fb2e5516daced4e62ac38c65f
atikurrahman33 <foysalatik33@gmail.com>
2023-09-27 22:42:14 +0600

    pie chart add

--- src/pages/statistic/Statistics.jsx
+++ src/pages/statistic/Statistics.jsx
@@ -1,33 +1,17 @@
-import { PieChart, Pie,  Tooltip,  } from 'recharts';
+import PieChartt from "./PieChartt";
+
+
 
 const Statistics = () => {
-    const data01 = [
-        { name: 'Group A', value: 400 },
-        { name: 'Group B', value: 300 },
-        { name: 'Group C', value: 300 },
-        { name: 'Group D', value: 200 },
-        { name: 'Group E', value: 278 },
-        { name: 'Group F', value: 189 },
-      ];
-     
-    return (
-        <div className='flex items-center jus'>
-             <PieChart width={400} height={400}>
-          <Pie
-            dataKey="value"
-            isAnimationActive={false}
-            data={data01}
-            cx="50%"
-            cy="50%"
-            outerRadius={80}
-            fill="#8884d8"
-            label
-          />
-          
-          <Tooltip />
-        </PieChart>
-        </div>
-    );
+  const donateItems = JSON.parse(localStorage.getItem('test'));
+  const item=donateItems?donateItems.length : 0;
+  
+
+  return (
+    <div>
+    <PieChartt item={item}></PieChartt>
+    </div>
+  );
 };
 
 export default Statistics;
